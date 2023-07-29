679cbf521e95f70b4ec2e7d7d4f7887de9c79017
The Briant Carrillo <brianttd24.7@gmail.com>
2023-07-28 23:49:31 -0400

    agregamos cambios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let header = document.getElementById('cabecera')
+window.addEventListener('scroll', ()=> {
+    let scroll = window.scrollY
+    if(scroll>10){
+        header.style.backgroundColor = 'transparent'
+        header.style.boxShadow = '0px 0px 10px 4px black'
+    } else{
+        header.style.backgroundColor = 'white'
 
+    }
+})
