43bbccdb0a3c6ad670f41911860a4b95e9b8761f
The Briant Carrillo <brianttd24.7@gmail.com>
2023-07-31 16:35:47 -0400

    agregamos pupop

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-let header = document.getElementById('cabecera')
-window.addEventListener('scroll', ()=> {
-    let scroll = window.scrollY
-    if(scroll>10){
-        header.style.backgroundColor = 'transparent'
-        header.style.boxShadow = '0px 0px 10px 4px black'
-    } else{
-        header.style.backgroundColor = 'transparent'
+function iniciarPagina() {
+  let degradado = document.getElementById('popup')
+  degradado.addEventListener('click', toggle);
 
-    }
-})
+  let cerrar = document.getElementById('cerrar-popup')
+  cerrar.addEventListener('click', toggle)
+}
+
+let header = document.getElementById("cabecera");
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+  if (scroll > 10) {
+    header.style.backgroundColor = "transparent";
+    header.style.boxShadow = "0px 0px 10px 4px black";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+function toggle() {
+  let perciana = document.getElementById('perciana');
+  perciana.classList.toggle('active')
+  let contactame = document.getElementById('contactame');
+  contactame.classList.toggle('active')
+}
+window.addEventListener("load", iniciarPagina);
