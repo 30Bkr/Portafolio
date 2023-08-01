298571b8774bdebbd2b7e4abcd2ee92e3fde5a73
The Briant Carrillo <brianttd24.7@gmail.com>
2023-08-01 14:27:37 -0400

    agregamos cambios

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+const slider = document.querySelector('.slider__contenedor')
+let slideSection = document.querySelectorAll('.slider__card')
+let sliderSectionLast = slideSection[slideSection.length -1];
+
+
+const slide1 = document.querySelector('.slide1')
+const slide2 = document.querySelector('.slide2')
+
+slide1.addEventListener('click', siguiente)
+slide2.addEventListener('click', anterior)
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+
+
+
+
+
 function iniciarPagina() {
   let degradado = document.getElementById('popup')
   degradado.addEventListener('click', toggle);
@@ -5,6 +22,39 @@ function iniciarPagina() {
   let cerrar = document.getElementById('cerrar-popup')
   cerrar.addEventListener('click', toggle)
 
+
+}
+function toggle() {
+  let perciana = document.getElementById('perciana');
+  perciana.classList.toggle('active')
+  let contactame = document.getElementById('contactame');
+  contactame.classList.toggle('active')
+}
+function siguiente(){
+ let sliderSectionFirst = document.querySelectorAll('.slider__card')[0];
+ slider.style.marginLeft = "-200%"
+ slider.style.transition = "all 0.5s"
+ setTimeout(function(){
+  slider.style.transition = 'none'
+  slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+ slider.style.marginLeft = "-100%"
+
+ },500)
+
+console.log('siguiente')
+}
+function anterior(){
+  let slideSection = document.querySelectorAll('.slider__card')
+  let sliderSectionLast = slideSection[slideSection.length -1];
+  slider.style.marginLeft = "0"
+  slider.style.transition = "all 0.5s"
+  setTimeout(function(){
+   slider.style.transition = 'none'
+   slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+  slider.style.marginLeft = "-100%"
+ 
+  },500)
+  console.log('anterior')
 }
 
 let header = document.getElementById("cabecera");
@@ -18,10 +68,4 @@ window.addEventListener("scroll", () => {
   }
 });
 
-function toggle() {
-  let perciana = document.getElementById('perciana');
-  perciana.classList.toggle('active')
-  let contactame = document.getElementById('contactame');
-  contactame.classList.toggle('active')
-}
 window.addEventListener("load", iniciarPagina);
